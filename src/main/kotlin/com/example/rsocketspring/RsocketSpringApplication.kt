c90f78028e5571d97177b716185c5cb5d25fbd71
Vadim Pogodin <pog120vad@gmail.com>
2022-12-02 12:10:16 +0300

    init project

---
+++ src/main/kotlin/com/example/rsocketspring/RsocketSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rsocketspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RsocketSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<RsocketSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/rsocketspring/RsocketSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.rsocketspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RsocketSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
